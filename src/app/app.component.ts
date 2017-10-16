6604824b647583dce684ef1f4e0afd0bd1cc22a7
g-todorov <georgi.todorov.19@gmail.com>
2017-10-16 11:44:11 +0300

    added footer and content.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,64 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   animations: [
     trigger('menuState', [
       state('menuActive', style({
-        top: '100px'
+        top: '50px'
       })),
       transition('* => menuActive', animate('200ms ease-in')),
-    ])
+    ]),
+    trigger('firstMenuItemState', [
+      state('nonActiveMenuItem', style({
+        width: '200px',
+        height: '200px'
+      })),
+      state('activeMenuItem', style({
+        transform: 'rotate(45deg)',
+        width: '150px',
+        height: '150px'
+      })),
+      transition ('nonActiveMenuItem <=> activeMenuItem', animate('200ms ease-in')),
+    ]),
+    trigger('secondMenuItemState', [
+      state('activeMenuItem', style({
+        transform: 'rotate(45deg)',
+        width: '150px',
+        height: '150px'
+      })),
+      transition ('nonActiveMenuItem <=> activeMenuItem', animate('200ms ease-in')),
+    ]),
+    trigger('thirdMenuItemState', [
+      state('activeMenuItem', style({
+        transform: 'rotate(45deg)',
+        width: '150px',
+        height: '150px'
+      })),
+      transition ('nonActiveMenuItem <=> activeMenuItem', animate('200ms ease-in')),
+    ]) 
   ]
 })
 export class AppComponent {
-  title = 'app';
   menuState: string = 'inactive'
+  firstMenuItemState: string = 'nonActiveMenuItem'
+  secondMenuItemState: string = 'nonActiveMenuItem'
+  thirdMenuItemState: string = 'nonActiveMenuItem'
 
-  activateMenu() {
+  onFirstMenuItemClicked() {
+    this.firstMenuItemState = 'activeMenuItem'
+    this.secondMenuItemState = 'nonActiveMenuItem'
+    this.thirdMenuItemState = 'nonActiveMenuItem'
+    this.menuState = 'menuActive'
+  }
+
+  onSecondMenuItemClicked() {
+    this.secondMenuItemState = 'activeMenuItem'
+    this.firstMenuItemState = 'nonActiveMenuItem'
+    this.thirdMenuItemState = 'nonActiveMenuItem'
+    this.menuState = 'menuActive'
+  }
+
+  onThirdMenuItemClicked() {
+    this.thirdMenuItemState = 'activeMenuItem'
+    this.firstMenuItemState = 'nonActiveMenuItem'
+    this.secondMenuItemState = 'nonActiveMenuItem'
     this.menuState = 'menuActive'
   }
 }
